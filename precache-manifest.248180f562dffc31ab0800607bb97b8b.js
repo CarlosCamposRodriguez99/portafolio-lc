3c9b6c9ff48837a7619e10cafa21259ed92931a2
Carlos Campos <jccamposrz@gmail.com>
2022-07-16 20:38:02 -0500

    Updates

---
+++ precache-manifest.248180f562dffc31ab0800607bb97b8b.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc4165eb3e6bde0c9d71ed567ed3071f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acb07f6ab7e24355e93a",
+    "url": "/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "a78a593fae1ff0ec2a74",
+    "url": "/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "8d2301955d57b96603dc",
+    "url": "/static/css/main.9a04cd56.chunk.css"
+  },
+  {
+    "revision": "465a657b6dd35d346647",
+    "url": "/static/js/2.f597b420.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.f597b420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acb07f6ab7e24355e93a",
+    "url": "/static/js/3.285b7bd4.chunk.js"
+  },
+  {
+    "revision": "a78a593fae1ff0ec2a74",
+    "url": "/static/js/4.9308d4d4.chunk.js"
+  },
+  {
+    "revision": "8d2301955d57b96603dc",
+    "url": "/static/js/main.a3b87ecb.chunk.js"
+  },
+  {
+    "revision": "f979ec10e95fba4490b1",
+    "url": "/static/js/runtime-main.041dcbf0.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "9fc7dcf914cfb6c115e2c292e6319762",
+    "url": "/static/media/bitcoin.9fc7dcf9.svg"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "cd625f744bef3b82cf4cb1436a06eb1e",
+    "url": "/static/media/ecommerce.cd625f74.svg"
+  },
+  {
+    "revision": "821221fe5cb997fbe49dfbfeb2b19b47",
+    "url": "/static/media/finanza.821221fe.svg"
+  },
+  {
+    "revision": "ddf3d68f1b89d5ca090950d0b663cb57",
+    "url": "/static/media/gaia.ddf3d68f.jpg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "7c64d4f7ea5070f290427a97f27b151f",
+    "url": "/static/media/graphql.7c64d4f7.png"
+  },
+  {
+    "revision": "a2fc1e46dad07b31b6c3f047dcf5e006",
+    "url": "/static/media/houses.a2fc1e46.svg"
+  },
+  {
+    "revision": "1f53e40d248cd143be304da86dd8a877",
+    "url": "/static/media/js.1f53e40d.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "a7212ed4c02ca28a98c310a947c21998",
+    "url": "/static/media/react.a7212ed4.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "f1ae5f119aa4d1bcb6df2dcdd3bda27f",
+    "url": "/static/media/udg.f1ae5f11.png"
+  }
+]);
